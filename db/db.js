ffc61ea3a388847b8c31dc2dde3ee6e89f5b2124
John Pickett <john.pickett@maryville.com>
2019-02-27 15:12:32 -0600

    Updated db config

--- db/db.js
+++ db/db.js
@@ -1,10 +1,12 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: 'postgres'
-});
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+	dialect: 'postgres',
+	ssl: true,
+	"dialectOptions": {
+		"ssl": true
+	}
+})
 
 const models = {
   User: sequelize.import('../models/user'),
